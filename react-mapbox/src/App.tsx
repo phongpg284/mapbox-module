b4fac88a01ba710df3d10f94d01f704095cdf2c0
Phong Phi Gia <tpw284@gmail.com>
2021-08-24 14:49:55 +0700

    update mapbox layers

--- react-mapbox/src/App.tsx
+++ react-mapbox/src/App.tsx
@@ -7,14 +7,6 @@ function App() {
   const mapRef = useRef()
   const [fieldData, setFieldData] = useState<any>();
   const handleSubmit = () => {
-    // let geoJSON;
-    // const oldJSONData = localStorage.getItem("fields");
-    // console.log(oldJSONData, fieldData, "ole")
-    // if (oldJSONData) {
-    //   const oldFieldData = JSON.parse(oldJSONData);
-    //   geoJSON = JSON.stringify([...oldFieldData, ...fieldData]);
-    // } else geoJSON = JSON.stringify(fieldData);
-    // localStorage.setItem("fields", geoJSON);
     (mapRef.current as any).saveData()
   };
 
--- react-mapbox/src/Mapbox/config.ts
+++ react-mapbox/src/Mapbox/config.ts
@@ -1,20 +1,28 @@
+// mapbox access token
 export const accessToken =
   "pk.eyJ1IjoidHB3Mjg0IiwiYSI6ImNrc2VrYnk0bjExaWIybnJveGFtOGV0eDAifQ.pSJ4eAaCbdrjhzmqXMRK_A";
 
+// default zoom and center
 export const defaultCenter: [number, number] = [-91.874, 42.76];
 export const defaultZoom: [number] = [12];
 
+// default polygon fill color when drawing
 export const polygonFillColor = "#00fd90";
 export const polygonFillOpacity = 0.7;
 
+// default line color when drawing
 export const lineColor = "#ff5050";
 export const lineWidth = 3.5;
 
+// default polygon color when display data on map
+export const showFieldDisplay = true
 export const fillPaint = {
   "fill-color": polygonFillColor,
   "fill-opacity": polygonFillOpacity,
 };
 
+// default line color when display data on map
+export const showLineDisplay = false
 export const linePaint = {
   "line-color": lineColor,
   "line-width": lineWidth,
