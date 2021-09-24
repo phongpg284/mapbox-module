2746d69a5b15a6c9d9d0ee330b5f4a371aa891ab
Phong Phí Gia <tpw284@gmail.com>
2021-09-24 17:56:14 +0700

    edit title

--- react-mapbox/src/index.tsx
+++ react-mapbox/src/index.tsx
@@ -1,10 +1,13 @@
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter } from "react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+
 
 ReactDOM.render(
   <React.StrictMode>
