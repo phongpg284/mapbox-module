4c30f978192e2b3474ed7e1c51139939b369ad9c
Trungnh <nguyen.hoang.trung@mqsolutions.com.vn>
2021-11-16 21:33:46 +0700

    edit login responsive

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { useMediaQuery } from 'react-responsive'
 import LoginMobile from './components/LoginMobile'
 
 function App() {
-    const isMobile = useMediaQuery({ query: `(max-width: 768px)` })
+    const isMobile = useMediaQuery({ query: `(max-width: 992px)` })
 
     return (
         <div className="App">
