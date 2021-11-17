91d96ad7da1d06dbdffc3b5e0c2999f7e6926fd4
Trungnh <nguyen.hoang.trung@mqsolutions.com.vn>
2021-11-17 10:02:45 +0700

    function menu css

--- src/pages/HomePage/HomeDashboard/HomeDashboard.tsx
+++ src/pages/HomePage/HomeDashboard/HomeDashboard.tsx
@@ -1,7 +1,7 @@
 import { useLocation } from 'react-router'
 
 import { Menu } from 'antd'
-import { UserOutlined, VideoCameraOutlined } from '@ant-design/icons'
+
 import { useAppSelector } from '../../../app/store'
 
 
@@ -15,86 +15,64 @@ const HomeDashboard = ({ selectItem }: any) => {
     }
     return (
         <div className="home-dashboard">
-            <Menu
-                className="function-menu"
-                mode="inline"
-                selectedKeys={[location.pathname]}
-                onClick={handleSelectMenuItem}
-            >
-                <Menu.ItemGroup>
-                    <Menu.Item className="function-menu-item" key="/">
-                        
-                        Trang chủ
-                    </Menu.Item>
-                    <Menu.Item className="function-menu-item" key="/settings">
-                        Cài đặt
-                    </Menu.Item>
-                </Menu.ItemGroup>
-
-                <Menu.ItemGroup className="function-menu-title" title="QUẢN LÍ DỰ ÁN">
-                    <Menu.Item className="function-menu-item" key="/projects/list"  >
-                        <div className="projects-icon"></div>
-                        Dự án
-                    </Menu.Item>
-                    <Menu.Item className="function-menu-item" key="/reports">
-                        <div className="reports-icon"></div>
-                        Báo cáo
-                    </Menu.Item>
-                </Menu.ItemGroup>
-                <Menu.ItemGroup className="function-menu-title" title="QUẢN LÍ THIẾT BỊ">
-                    <Menu.Item className="function-menu-item" key="/active-devices/list">
-                        <div className="active-devices-icon"></div>
-                        Thiết bị hoạt động
-                    </Menu.Item>
-                    <Menu.Item className="function-menu-item" key="/devices/list">
-                        <div className="devices-icon"></div>
-                        Danh sách thiết bị
-                    </Menu.Item>
-                    <Menu.Item
-                        className="function-menu-item"
-                        key="/machines/list"
-                    >
-                        <div className="machines-icon"></div>
-                        Danh sách máy móc
-                    </Menu.Item>
-                    {/* <Menu.Item key="menu_3_3" icon={<VideoCameraOutlined />}>
-                        Dữ liệu vòng tua
-                    </Menu.Item>
-                    <Menu.Item key="menu_3_4" icon={<VideoCameraOutlined />}>
-                        Dữ liệu toàn đạc
-                    </Menu.Item> */}
-                </Menu.ItemGroup>
-
-                {account.role === 'moderator' && (
-                    <>
-                        <Menu.ItemGroup className="function-menu-title"  title="QUẢN LÍ NHÂN SỰ">
-                            {/* <Menu.Item key="/moderators/list" icon={<UserOutlined />}>
-                                Quản trị viên
-                            </Menu.Item> */}
-                            <Menu.Item
-                                className="function-menu-item"
-                                key="/users/list"
-                            >
-                                Người dùng
-                            </Menu.Item>
-                        </Menu.ItemGroup>
-                        <Menu.ItemGroup>
-                            <Menu.SubMenu title="Lịch sử track">
-                                <Menu.Item
-                                    key="/fields/list"
-                                >
-                                    List
-                                </Menu.Item>
-                                <Menu.Item
-                                    key="/fields/card"
-                                >
-                                    Cards
-                                </Menu.Item>
-                            </Menu.SubMenu>
-                        </Menu.ItemGroup>
-                    </>
-                )}
-            </Menu>
+            <ul className="function-menu">
+                <li className="function-menu-group">
+                    <div className="group-title">Quản lý dự án</div>
+                    <ul className="group-list">
+                        <li className="group-item">
+                            <div className="item-icon projects"></div>
+                            <div className="item-title">Dự án</div>
+                        </li>
+                        <li className="group-item">
+                            <div className="item-icon reports"></div>
+                            <div className="item-title">Báo cáo</div>
+                        </li>
+                    </ul>
+                </li>
+                <li className="function-menu-group">
+                    <div className="group-title">Quản lý thiết bị</div>
+                    <ul className="group-list">
+                        <li className="group-item">
+                            <div className="item-icon active-devices"></div>
+                            <div className="item-title">Thiết bị hoạt động</div>
+                        </li>
+                        <li className="group-item">
+                            <div className="item-icon devices-list"></div>
+                            <div className="item-title">Danh sách thiết bị</div>
+                        </li>
+                        <li className="group-item">
+                            <div className="item-icon machine-list"></div>
+                            <div className="item-title">Danh sách máy móc</div>
+                        </li>
+                    </ul>
+                </li>
+                <li className="function-menu-group">
+                    <div className="group-title">Quản lý nhân sự</div>
+                    <ul className="group-list">
+                        <li className="group-item">
+                            <div className="item-icon admins"></div>
+                            <div className="item-title">Quản trị viên</div>
+                        </li>
+                        <li className="group-item">
+                            <div className="item-icon users"></div>
+                            <div className="item-title">Người dùng</div>
+                        </li>
+                    </ul>
+                </li>
+                <li className="function-menu-group">
+                    <div className="group-title">Lịch sử track</div>
+                    <ul className="group-list">
+                        <li className="group-item">
+                            <div className="item-icon lists"></div>
+                            <div className="item-title">Lists</div>
+                        </li>
+                        <li className="group-item">
+                            <div className="item-icon cards"></div>
+                            <div className="item-title">Cards</div>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
         </div>
     )
 }
