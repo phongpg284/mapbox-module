9ded9c91529095c0318c37e015a1b34c0dc45cc6
Phong Phí Gia <tpw284@gmail.com>
2021-11-23 15:06:19 +0700

    update bootstrap

--- src/components/Map/RecordMap/customTooltip.ts
+++ src/components/Map/RecordMap/customTooltip.ts
@@ -1,8 +1,22 @@
+import dayjs from 'dayjs'
+
 const customTooltip = (params: any[]) => {
-    let renderString = ""
+    let renderString = ''
     params.forEach((line) => {
-        const newRenderString = `<div style="text-align: start"> <span style="font-weight: bold; font-family: sans-serif"> ${(line?.seriesName)} </span>: ${line?.value?.toFixed(2)} ${getUnit(line?.seriesName) || ""} </div> `
-      renderString = renderString.concat(newRenderString)
+        if (line?.seriesName === 'Thời gian') {
+            console.log(line?.value)
+            const newRenderString = `<div style="text-align: start"> <span style="font-weight: bold; font-family: sans-serif"> ${line?.seriesName} </span>: ${dayjs
+                .utc(line?.value, 'DDMMYYHHmmss')
+                .local()
+                .format('DD/MM/YY HH:mm:ss')} </div> `
+
+            renderString = renderString.concat(newRenderString)
+        } else {
+            const newRenderString = `<div style="text-align: start"> <span style="font-weight: bold; font-family: sans-serif"> ${line?.seriesName} </span>: ${line?.value?.toFixed(
+                2
+            )} ${getUnit(line?.seriesName) || ''} </div> `
+            renderString = renderString.concat(newRenderString)
+        }
     })
     return renderString
 }
@@ -20,18 +34,17 @@ const customTooltip = (params: any[]) => {
 //   }
 // }
 
-
 const getUnit = (key: string) => {
-  switch (key) {
-    case "Quãng đường":
-      return "m"
-    case "Vận tốc":
-      return "km/h"
-    case "Độ chính xác":
-      return "cm"
-    default:
-      break;
-  }
+    switch (key) {
+        case 'Quãng đường':
+            return 'm'
+        case 'Vận tốc':
+            return 'km/h'
+        case 'Độ chính xác':
+            return 'cm'
+        default:
+            break
+    }
 }
 
 export default customTooltip
